9781d21dddb9a6f9004c264dc7b2e59f2df9b174
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-04-18 10:22:54 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
-img.shields.io/badge/License-[license name]-lightgrey
+![]()
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
-choosealicense.com/licenses/[license name]/
+[]()
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
